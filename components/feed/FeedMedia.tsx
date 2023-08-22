166cc236c18a9a2578505403eeef8a122c23008e
codebyps <priyanshu.creator.dev@gmail.com>
2023-08-22 06:10:14 +0530

    rounded image

--- components/feed/FeedMedia.tsx
+++ components/feed/FeedMedia.tsx
@@ -37,19 +37,19 @@ const FeedMedia = ({ medias }: { medias: Media[] }) => {
     <div ref={sliderRef} className="keen-slider zoom-out">
       {medias?.map((image, idx) => {
         return (
-          <div className={`keen-slider__slide zoom-out__slide`}>
-            <div style={scaleStyle(idx)}>
+          <div className={`keen-slider__slide zoom-out__slide rounded-lg`}>
+            <div style={scaleStyle(idx)} className="">
               <Image
                 key={idx}
                 src={imageUrlCloudinary(image)}
                 alt={`${image.public_id} preview ${idx}`}
                 priority={idx === 0}
-                className="rounded-lg shadow-lg p-2 object-cover"
+                className="rounded-lg shadow-lg object-cover"
                 placeholder="empty"
                 height={300}
                 width={350}
               />
-              <div>
+              <div className="pt-2">
                 <span className="text-center text-neutral-600 p-2">
                   {idx + 1} of {medias.length}
                 </span>
